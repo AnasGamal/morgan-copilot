ffe0ea43f3c9a08935082d2b3c47f7870aada243
Jawa <joshuasingh076@gmail.com>
2023-10-07 01:20:05 -0400

    update password

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dbUser:<password>@cluster0.lwvx13a.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dbUser:<dbUserPassword>@cluster0.lwvx13a.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
